7e4c728ef3cdc8d38849c4022d9685af3af8a6a3
Francisco Diez <frandiezdev@gmail.com>
2022-03-08 16:55:05 -0300

    Ejercicio paracaidista

--- u1/paracaidista.py
+++ u1/paracaidista.py
@@ -1,1 +1,21 @@
-print("Primer comentario")
+import matplotlib.pyplot as plt
+
+m = 68.1
+c = 12.5
+g = 9.8
+ti = 0
+tf = 12
+dt = 2 # Este es el "paso" que yo tomo
+
+n = int(((tf-ti)/dt))
+print("n: "+ str(n))
+v = [0.0]*n
+t = [0.0]*n
+
+for i in range(n-1):
+    t[i+1] = t[i] + dt
+    v[i+1] = v[i]+g-(c/m*v[i])*dt
+    print(t[i+1], v[i+1])
+
+plt.plot(t, v)
+plt.show()
