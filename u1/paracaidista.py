5d155b255bcf73bc021470acde9fe51ee90193c6
Francisco Diez <frandiezdev@gmail.com>
2022-03-10 18:01:07 -0300

    se agregan cambios en la formula de velocidad, para que este mas parecida a la de las filminas

--- u1/paracaidista.py
+++ u1/paracaidista.py
@@ -6,23 +6,18 @@ c = 12.5
 g = 9.8
 ti = 0
 tf = 12
-dt = 2 # Este es el "paso" que yo tomo
+dt = 2 # Este es el "paso" que tomo
 
 n = int(((tf-ti)/dt))
-print("n: "+ str(n))
+# print("n: "+ str(n))
 
-# Creamos dos vectores para v y t con dimension n
-# luego en el ciclo for modificamos indice a indice
-# v = [0.0]*n
-# t = [0.0]*n
-# Esto es una forma alternativa de crear los vectores de dimension n
 v = np.zeros(n)
 t = np.zeros(n)
 
 for i in range(n-1):
     print(i)
     t[i+1] = t[i] + dt
-    v[i+1] = v[i]+g-(c/m*v[i])*dt
+    v[i+1] = v[i]+g-(c/m*v[i])*(t[i+1]-t[i])
     print(t[i+1], v[i+1])
 
 plt.plot(t, v)
