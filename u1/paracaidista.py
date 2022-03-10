2604e82afe5df50ff8d09d6478f870a945de86e0
Francisco Diez <frandiezdev@gmail.com>
2022-03-10 10:19:16 -0300

    comentarios para entender mejor

--- u1/paracaidista.py
+++ u1/paracaidista.py
@@ -9,10 +9,14 @@ dt = 2 # Este es el "paso" que yo tomo
 
 n = int(((tf-ti)/dt))
 print("n: "+ str(n))
+
+# Creamos dos vectores para v y t con dimension n
+# luego en el ciclo for modificamos indice a indice
 v = [0.0]*n
 t = [0.0]*n
 
 for i in range(n-1):
+    print(i)
     t[i+1] = t[i] + dt
     v[i+1] = v[i]+g-(c/m*v[i])*dt
     print(t[i+1], v[i+1])
