6f3bdeac0d381261950371ffaf2a2dd36577bbfe
Francisco Diez <frandiezdev@gmail.com>
2022-03-15 13:27:02 -0300

    ahora si funciona

--- u2/biseccion.py
+++ u2/biseccion.py
@@ -1,8 +1,9 @@
 from math import *
 
+# Definimos la funcion
+
 eqn = str(input("Ingrese ecuacion: "))
 
-# Definimos la funcion
 def f(x):
     return eval(eqn)
 
@@ -11,25 +12,27 @@ def f(x):
 # t -> tolerancia ???
 def biseccion(a, b, t):
 
+    n1=a
+    n0=b
+
     k = 0
-    m = f(a)*f(b)
-    
-    if(m>0):
+
+    if(f(a)*f(b)>0):
         print("En el intervalo no hay cambio de signo")
     else:
+        while (abs(n1-n0)>=t):
+            n1 = n0
+            n0 = (a+b)/2
 
-        while abs(a-b) > t:
-            xR = (a+b)/2
-            
-            if(m<0): # Cambia de signo en [a,xR]
-                b = xR # El nuevo limite superior es el valor medio
-            if(m>0): # Cambia de signo en [xR,b]
-                a = xR # El nuevo limite inferior es el valor medio
+            if(f(a)*f(n0)<0): # Cambia de signo en [a,n0]
+                b = n0 # El nuevo limite superior es el valor medio
+            if(f(n0)*f(b)<0): # Cambia de signo en [n0,b]
+                a = n0 # El nuevo limite inferior es el valor medio
 
-            print("La raiz esta en el subintervalo [",a,",",b,"] ")
+            print("[",a,",",b,"] ")
             k = k+1
-        
-        print("La mejor aproximacion a la raiz es x",k,"=",xR)
+            
+        print("La mejor aproximacion a la raiz es x",k,"=",n0)
 
 xL = float(input("Limite superior: "))
 xU = float(input("Limite inferior: "))
